d19b9ccba854357e1b10d87d2c1d73f1ed0e8c1e
somewhater <guowei400@gmail.com>
2015-12-19 01:59:59 +0800

    单例验证

--- app/src/main/java/com/lgw/androidh5/Fragment_one.java
+++ app/src/main/java/com/lgw/androidh5/Fragment_one.java
@@ -5,14 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 /**
  * Created by lgw on 2015-12-18.
  */
 public class Fragment_one extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_one, container, false);
     }
 }
