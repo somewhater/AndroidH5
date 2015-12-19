13e0c67d186072b6d290b57040a4d47a067c7257
somewhater <guowei400@gmail.com>
2015-12-19 10:50:57 +0800

    功能初步完成

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-﻿#Android混合Html5样板工程
-##添加四个Button，整理布局，动态添加四个fragment，并设置监听
+﻿#Android混合Html5样板工程  
+##添加四个Button，整理布局，动态添加四个fragment，并设置监听  
+##监听单例模式化  
+##给fragment添加Url  
+##设置网页在WebView中开启  
+#END
--- app/src/main/java/com/lgw/androidh5/Fragment_one.java
+++ app/src/main/java/com/lgw/androidh5/Fragment_one.java
@@ -1,10 +1,14 @@
 package com.lgw.androidh5;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 /**
  * Created by lgw on 2015-12-18.
  */
@@ -12,5 +16,26 @@ public class Fragment_one extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        WebView myWebView = (WebView) getView().findViewById(R.id.wv_one);
+        myWebView.loadUrl("http://www.baidu.com");
+        myWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+        });
     }
 }
